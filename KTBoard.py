a695558105e2aeaca559faddde77b0d8e29313ae
Fuchs RLTN <fuchs@rltn.de>
2024-12-01 18:04:23 +0100

    2

--- KTBoard.py
+++ KTBoard.py
@@ -2,4 +2,17 @@ import streamlit as st
 
 st.title('Kill Team SCOREBOARD')
 
+# Setting initial scores
+score_p1_crit = 1
+score_p1_kill = 1
+score_p1_secr = 1
+
+
+
+# Presenting initial scores
+
 st.write("score")
+st.write(score_p1_crit)
+st.write(score_p1_kill)
+st.write(score_p1_crit)
+
