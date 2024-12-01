e067a47b5d576831d94af0c73fb71af49494aabe
Fuchs RLTN <fuchs@rltn.de>
2024-12-01 17:37:27 +0100

    POC - Scoring with pills 2

--- KTBoard.py
+++ KTBoard.py
@@ -6,6 +6,6 @@ st.title('Kill Team SCOREBOARD')
 
 
 points = ["Critical", "Kill", "Secret"]
-selection = st.pills("Scoerd for", options, selection_mode="multi")
+selection = st.pills("Scoerd for", points, selection_mode="multi")
 st.markdown(f"Your selected options: {selection}.")
 
