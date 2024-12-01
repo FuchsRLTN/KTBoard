40b9079d3c673c632c91f6db287bbc49787e711b
Fuchs RLTN <fuchs@rltn.de>
2024-12-01 17:57:55 +0100

    1

--- KTBoard.py
+++ KTBoard.py
@@ -2,26 +2,4 @@ import streamlit as st
 
 st.title('Kill Team SCOREBOARD')
 
-score_party1_critical = 0
-score_party1_kill = 0
-score_party1_secret = 0
-
-
-st.write("ergebnis:")
-st.write(score_party1_critical)
-
-
-
-points = ["Critical", "Kill", "Secret"]
-selection = st.pills("Scored for", points, selection_mode="single")
-
-
-##if selection in ["Critical"]:
-
-score_party1_critical = score_party1_critical +1
-##else:
-##    print("nix")
-
-
-
-
+st.write("score")
